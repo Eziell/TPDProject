8e102d07b1b2915b5bd98c6e7385a7e8a1d54c6d
Jorge Cruz <jorge.viana.cruz3010@hotmail.com>
2019-05-24 19:28:51 +0100

    rf_classifier update

--- rf_classifier_number_of_teams.py
+++ rf_classifier_number_of_teams.py
@@ -1,46 +1,76 @@
 import pandas as pd
-
+import numpy as np
 dataset = pd.read_csv('TPD-CSV/dados_prospecao.csv', encoding='cp850')
 to_predict = pd.read_csv('TPD-CSV/dados_previsao.csv', encoding='cp850')
-to_predict = to_predict.iloc[:, 3:9].values
-new_data = []
-for i in range(len(dataset['equipas_seguinte'])):
-    if dataset['equipas_seguinte'][i] < 7:
-         new_data.append(str(dataset['equipas_seguinte'][i]))
+new_data_teams = []
+new_data_teams_following_season = []
+for i in range(len(dataset['equipas_epoca_seguinte'])):
+    if dataset['equipas_epoca_seguinte'][i] < 7:
+         new_data_teams_following_season.append(str(dataset['equipas_epoca_seguinte'][i]))
     else:
-        new_data.append('>6')
+        new_data_teams_following_season.append('>6')
         
-dataset['class']=new_data
+for i in range(len(dataset['equipas'])):
+    if dataset['equipas'][i] < 7:
+         new_data_teams.append(str(dataset['equipas'][i]))
+    else:
+        new_data_teams.append('>6')
+    
+dataset['prev_class']=new_data_teams
+dataset['class']=new_data_teams_following_season
+del dataset['equipas_epoca_seguinte']
+del dataset['equipas']
+new_data_teams = []
+        
+for i in range(len(to_predict['equipas'])):
+    if to_predict['equipas'][i] < 7:
+         new_data_teams.append(str(to_predict['equipas'][i]))
+    else:
+        new_data_teams.append('>6')
+        
 dataset['gender']=[x.strip() for x in dataset['gender']]
 
-X = dataset.iloc[:, 3:9].values  
-y = dataset.iloc[:, 11].values
-
-print(y)
+to_predict['prev_class']=new_data_teams
+del to_predict['equipas']
 
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
-for i in range(6):
-    X[:,i] = le.fit_transform(X[:,i])
-y=le.fit_transform(y)
-for i in range(6):
-    to_predict[:,i] = le.fit_transform(to_predict[:,i])
+dataset['agegroup']=le.fit_transform(dataset['agegroup'])
+dataset['gender']=le.fit_transform(dataset['gender'])
+dataset['club']=le.fit_transform(dataset['club'])
+dataset['district']=le.fit_transform(dataset['district'])
+dataset['prev_class']=le.fit_transform(dataset['prev_class'])
+
+to_predict['agegroup']=le.fit_transform(to_predict['agegroup'])
+to_predict['gender']=le.fit_transform(to_predict['gender'])
+to_predict['club']=le.fit_transform(to_predict['club'])
+to_predict['district']=le.fit_transform(to_predict['district'])
+to_predict['prev_class']=le.fit_transform(to_predict['prev_class'])
+
+X = dataset.iloc[:, 0:7].values  
+y = dataset.iloc[:, 7].values
+to_predict = to_predict.iloc[:,:].values
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 from sklearn.preprocessing import StandardScaler
-
-sc = StandardScaler()  
-X_train = sc.fit_transform(X_train)  
-X_test = sc.transform(X_test)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 from sklearn.ensemble import RandomForestClassifier
 
-classifier = RandomForestClassifier(n_estimators=100, random_state=0)  
+classifier = RandomForestClassifier(n_estimators=100)  
 classifier.fit(X_train, y_train)  
 y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn import metrics
+pscore = metrics.accuracy_score(y_test, y_pred)
+print(pscore)
+print(pd.crosstab(y_test, y_pred, rownames=['Actual Class'], colnames=['Predicted Class']))
 
 predictions = classifier.predict(to_predict)
 print(predictions)
